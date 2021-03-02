88095084e86be7df58d40e74d1c82d1558eb0f90
Anna <67124154+Anna-2901@users.noreply.github.com>
2021-03-02 11:51:15 +0200

    add settings

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       hjncghjm
+      </header>
+    </div>
+  );
+}
+
+export default App;
